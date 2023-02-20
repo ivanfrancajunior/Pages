9d487652e488d4a9d935d81d118828d5df61eb33
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-02-20 11:28:05 -0300

    add  and extends props  to tailwindconfig file

--- Statics/Spotfy Home/tailwind.config.js
+++ Statics/Spotfy Home/tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        '14' : '3.5rem',
+        '125px':'125px'
+      },
+      colors:{
+        'hoverpt':'#18d760',
+        'purple-main':'#2d46b9',
+        'green-main':'#1ed760'
+      },
+      backgroundImage: theme =>({
+        'spotfy-theme':'url("/assets/bursts.svg")',
+        'spotfy-theme-mobile':'url("/assets/bursts-mobile.svg")',
+      }),
+      backgroundSize:{
+        '175%':'175%',
+        '195%': '195%'
+      },
+      backgroundPosition:{
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize:{
+        '9xl':'9rem'
+      }
+    },
   },
   plugins: [],
 }
