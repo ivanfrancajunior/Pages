7a8194ae5c29e9615daaf7e1b0cda2db860213ee
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-03-07 11:18:05 -0300

    adicionado componente de filme individual e fim

--- SPA/movie-lib/src/pages/Search/Search.jsx
+++ SPA/movie-lib/src/pages/Search/Search.jsx
@@ -32,7 +32,9 @@ const Search = () => {
             Resultados para: <span className="query-text">{query}</span>
           </h2>
           <div className="movies-container">
-          
+          {movies.length > 0 &&
+        movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      
             
           </div>
         </div>
