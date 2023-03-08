fe00fb6b2ba282024dbbf6bee25a3b963b8f8a49
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-03-08 00:37:05 -0300

    add personal portifolio

---
+++ Statics/personal-portifolio/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '500px': '500px', 
+        '376px': '376px',
+        '330px': '330px',
+        '340px': '340px',
+        '277px': '277px',
+        '210px': '210px',
+        '158px': '158px', 
+        '124px': '124px',
+        '106px': '106px',
+        '50px' : '50px', 
+        '10px' : '10px', 
+      },
+      fontFamily: {
+       'Poppins': 'Poppins',
+      }
+    },
+  },
+  plugins: [],
+}
