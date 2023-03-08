f334e59a226239384955b2a4695d8445c41a8f75
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-03-08 09:44:41 -0300

    finalizado projeto!

--- Statics/personal-portifolio/tailwind.config.js
+++ Statics/personal-portifolio/tailwind.config.js
@@ -4,19 +4,7 @@ module.exports = {
   darkMode:'class',
   theme: {
     extend: {
-      spacing: {
-        '500px': '500px', 
-        '376px': '376px',
-        '330px': '330px',
-        '340px': '340px',
-        '277px': '277px',
-        '210px': '210px',
-        '158px': '158px', 
-        '124px': '124px',
-        '106px': '106px',
-        '50px' : '50px', 
-        '10px' : '10px', 
-      },
+    
       fontFamily: {
        'Poppins': 'Poppins',
       }
