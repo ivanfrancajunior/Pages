538a8b6b4e56c741a10047cd2126342b03a55a4e
Ivan França <contato.ivanfrancajr@gmail.com>
2023-03-30 13:10:13 -0300

    change font type

--- PWA/secret-word-game/tailwind.config.cjs
+++ PWA/secret-word-game/tailwind.config.cjs
@@ -3,11 +3,11 @@ module.exports = {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily:{
+      'Delicious Handrawn':'Delicious Handrawn',
+    },
     
     extend: {
-      fontFamily:{
-        'NotoSansDisplay':'Noto Sans Display' 
-      }
     },
   },
   plugins: [],
