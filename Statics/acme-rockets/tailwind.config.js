8f7562faa8a4d1e6246d843c59bdb01e6063165e
Ivan Junior <contato.ivanfrancajr@gmail.com>
2023-05-13 17:38:58 -0300

    feat: add js files + end project

--- Statics/acme-rockets/tailwind.config.js
+++ Statics/acme-rockets/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./build/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'windescreen':{'raw':'(min-aspect-radio: 3/2)'},
+        'tallscreen':{'raw':'(min-aspect-radio: 16/20)'},
+  
+      }
+    },
+    keyframes: {
+      'open-menu': {
+        '0%':{transform: 'scaleY(0)'}, 
+        '80%':{transform: 'scaleY(1.2)'}, 
+        '100%':{transform: 'scaleY(1)'}, 
+      },
+    },
+    animation: {
+      'open-menu':'open-menu 0.4s ease-in-out forwards', 
+    }
   },
   plugins: [],
 }
