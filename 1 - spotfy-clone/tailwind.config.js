e09ac3ceaebd4032c10a224d825d304b9e029766
Ivan <contato.ivanfrancajr@gmail.com>
2023-11-01 11:09:12 -0300

    feat: adicionado spotfy-clone

---
+++ 1 - spotfy-clone/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing:{
+        '14': '3.5rem',
+        '125px':'125px'
+      },
+      colors:{
+        'hoverpt':'#18d760',
+        'purple-main':'#2d46b9',
+        'green-main':'#1ed760'
+      },
+      backgroundImage: theme =>({
+        'spotify-theme': "url('../img/bursts.svg')",
+        'spotify-theme-mobile':"url('../img/burst-mobile.svg')"
+      }),
+      backgroundSize:{
+        '175%':'175%',
+        '195%': '195%'
+      },
+      backgroundPosition:{
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize:{
+        '9xl':'9rem'
+      }
+    },
+  },
+  plugins: [],
+}
